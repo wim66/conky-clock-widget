6bed620a2e4b87af565669910505ab89c9b2ca51
wim66 <wimnet@gmail.com>
2017-01-02 18:15:07 +0100

    updated preview img

---
+++ conky-clock-widget (exemplaar)/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#sleep 30
+#killall conky
+sleep 1
+echo "\nstarting conky's\n"
+sleep 1
+( set -x; conky -c conkybg.conf )
+( set -x; conky -c conky.conf )
+echo "\nexiting"
+sleep 3
+
